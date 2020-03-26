ccf520890c2f5380e3e722cca1ef177571271e1a
Viu Long Kong <viulong.kong@fr.ibm.com>
2020-03-26 10:00:46 +0100

    Added readme + doc

---
+++ src/test/examples/UCanAccess/README.md
@@ -0,0 +1,3 @@
+This directory contains a small test run using UCanAccess.
+
+This is mainly to check that DOOPL works with UCanAccess (which it does)
--- src/test/examples/UCanAccess/run.bat
+++ src/test/examples/UCanAccess/run.bat
@@ -1,5 +1,7 @@
 @echo off
 
+REM This is an example using ODMS_JAVA_ARGS to set up classpath and jdbc.drivers
+
 set UCANPATH=%cd%\ucanaccess-5.0.0.jar
 set UCANPATH=%UCANPATH%;%cd%\commons-lang3-3.8.1.jar
 set UCANPATH=%UCANPATH%;%cd%\commons-logging-1.2.jar
